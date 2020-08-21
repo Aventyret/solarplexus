4aa2873fce231e7e5a5a95cb0068bfac5cae7b36
Andreas Bohman <mail@andreasbohman.se>
2020-08-21 15:27:19 +0200

    feature to override config from theme

--- class-ruled_display_block-helpers.php
+++ class-ruled_display_block-helpers.php
@@ -3,7 +3,15 @@
 class Ruled_display_block_Helpers {
   public static function retrieve_block_config() {
 
-    $config_path = RDB_PLUGIN_PATH . 'blockconfig.json';   
+    $config_path = RDB_PLUGIN_PATH . 'rdb-config.json';
+
+    $theme_config_path = get_stylesheet_directory() . '/rdb-config.json';
+
+    // Override with custom config from theme
+    if(file_exists($theme_config_path)) {
+      $config_path = $theme_config_path;
+    }
+      
     $json = file_get_contents( $config_path );
     $config_data = json_decode( $json, true );
 
