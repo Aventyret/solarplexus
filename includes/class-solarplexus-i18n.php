59b167fd41033a13785c2b0bd2052d15a585b509
Filip Wastman <fwastman@gmail.com>
2021-04-21 15:37:03 +0200

    Add Swedish translations

--- includes/class-solarplexus-i18n.php
+++ includes/class-solarplexus-i18n.php
@@ -35,7 +35,7 @@ class Solarplexus_i18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			'solarplexus',
+			'splx',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
--- solarplexus.php
+++ solarplexus.php
@@ -21,7 +21,7 @@
  * Author URI:        https://aventyret.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       solarplexus
+ * Text Domain:       splx
  * Domain Path:       /languages
  */
 
