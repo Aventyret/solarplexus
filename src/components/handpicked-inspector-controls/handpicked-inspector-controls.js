362bdd6b6be92ba37bf915aeb21f5c3e77ac5b8c
Per Arnborg <kontakt@perarnborg.se>
2022-03-31 11:30:27 +0200

    Support allowedPostTypes for handpicked blocks

--- src/components/handpicked-inspector-controls/handpicked-inspector-controls.js
+++ src/components/handpicked-inspector-controls/handpicked-inspector-controls.js
@@ -25,10 +25,45 @@ const HandpickedInspectorControls = ({ attributes, setAttributes, config, setIsD
   const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
+    const postTypes = config.allowedPostTypes ? config.allowedPostTypes : [];
+    const searchUris = [];
+    if (!postTypes.length) {
+      // Default to search endpoint if no post types are specified
+      searchUris.push(`/wp/v2/search/?search=${searchInput}`);
+    }
+    postTypes.forEach((postType) => {
+      // Otherwise do one search for each post type
+      const endpoint = `${postType}${['post', 'page'].includes(postType) ? 's' : ''}`
+      searchUris.push(`/wp/v2/${endpoint}/?search=${searchInput}`);
+    });
     const search = async () => {
-      const res = await apiFetch( {
-        path: `/wp/v2/search/?search=${searchInput}`
-      });
+      const res = await new Promise((resolve, reject) => {
+        Promise.all(searchUris.map((searchUri) => {
+          return apiFetch( { path: searchUri })
+        }))
+          .then((results) => {
+            const allResults = results.reduce((all, r, i) => {
+              return [
+                ...all,
+                ...r.map(p => {
+                  // If post type endpoints are used we need to map the results, but not for search endpoint
+                  if (!postTypes.length) {
+                    return p
+                  }
+                  return {
+                    id: p.id,
+                    title: p.title.rendered,
+                    url: p.link,
+                    type: postTypes[i],
+                    subtype: postTypes[i]
+                  }
+                })
+              ]
+            }, [])
+            resolve(allResults.slice(0, 10))
+          })
+          .catch((error) => reject(error))
+      })
 
       setSearchResults(res);
     };
