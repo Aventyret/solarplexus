988205ee5546e846ef06ad9da7edc80a132341c4
Per Arnborg <kontakt@perarnborg.se>
2022-11-02 23:00:42 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
