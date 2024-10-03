5a6494620fc3db3e17f331c9cd9e6fc38247a229
Per Arnborg <kontakt@perarnborg.se>
2024-10-03 22:53:18 +0200

    v1.18.0

--- solarplexus.php
+++ solarplexus.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Solarplexus
  * Plugin URI:        https://aventyret.com
  * Description:       A plugin and developer tool to easily create Gutenberg blocks for creating dynamic and static lists of posts, pages and more.
- * Version:           1.17.1
+ * Version:           1.18.0
  * Author:            Äventyret
  * Author URI:        https://aventyret.com
  * License:           GPL-2.0+
@@ -39,7 +39,7 @@ define('SPLX_API_BASE', 'splx/v1');
  * Current plugin version.
  * We use SemVer - https://semver.org
  */
-define('SOLARPLEXUS_VERSION', '1.17.0');
+define('SOLARPLEXUS_VERSION', '1.18.0');
 
 /**
  * The core plugin class that is used to define internationalization,
